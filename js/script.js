0665404d8a022b9760613170d022ad9379a4aa56
Russell grant Johnson <932400859@bun2-206-14.local>
2019-08-22 15:17:09 +1200

    added jquery

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 console.log('my script is working!!!');
 
+$('.reveal').hide();
+
 $(document).ready(function(){
 
 	// Opens the hamburger menu
@@ -17,4 +19,25 @@ $(document).ready(function(){
 		$('nav').fadeOut(500);
 	})
 
-});
+	// When button is clicked, a paragraph is revelaed
+	// $('.btn').click(function(){
+	// 	$('.reveal').show();
+	// 	$(this).hide();
+	// })
+	// $('#hide').click(function(){
+	// 	$('.reveal').hide();
+	// 	$('.btn').show();
+	// })
+	$('.btn').click(function(){
+		$('.reveal').slideToggle(2000);
+	})
+
+
+	// Super suitable for a sticky nav
+	$('#varibles').click(function(){
+		$('html,body').animate({
+			scrollTop: $('.uOL').offset().top}, 1000);
+		})
+
+
+}); //Document ready ends ---------
